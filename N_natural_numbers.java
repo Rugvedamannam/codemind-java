7b4f314e1ef31cd9486b581bf698005e3f963104
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2023-08-14 21:11:31 +0530

    import java.util.Scanner; public class sample{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         int i,j,s;         s=n.nextInt();         for(i=1;i<=s;i++)         {             System.out.printf("%d ",i);         }     } }

---
+++ N_natural_numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class sample{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        int i,j,s;
+        s=n.nextInt();
+        for(i=1;i<=s;i++)
+        {
+            System.out.printf("%d ",i);
+        }
+    }
+}
