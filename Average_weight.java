d0d16af6c9c5c9894e1141535b19a56d738ffa14
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2023-08-15 21:33:47 +0530

    import java.util.Scanner; public class sample{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         int a,b,c,d;         a=n.nextInt();         b=n.nextInt();         d=n.nextInt();         c=(3*a)-(b+d);         System.out.print(c);
        }
    }

---
+++ Average_weight.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class sample{
+    public static void main(String[] args)
+    {
+        Scanner n=new Scanner(System.in);
+        int a,b,c,d;
+        a=n.nextInt();
+        b=n.nextInt();
+        d=n.nextInt();
+        c=(3*a)-(b+d);
+        System.out.print(c);
+        
+    }
+}
