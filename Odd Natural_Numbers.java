8bb2dc2cf240d23486ba98fa4acd8564724b938c
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2023-08-24 21:02:49 +0530

    import java.util.Scanner; public class sample{     public static void main(String[] args)     {         Scanner n=new Scanner(System.in);         int s=n.nextInt();         for(int i=1;i<=s;i++)         {             if(i%2!=0)             {                 System.out.printf("%d ",i);             }         }     } }

--- Odd Natural_Numbers.java
+++ Odd Natural_Numbers.java
@@ -3,12 +3,13 @@ public class sample{
     public static void main(String[] args)
     {
         Scanner n=new Scanner(System.in);
-        int i,s;
-        s=n.nextInt();
-        for(i=1;i<=s;i++)
+        int s=n.nextInt();
+        for(int i=1;i<=s;i++)
         {
-           if(i%2!=0)
-           System.out.printf("%d ",i);
+            if(i%2!=0)
+            {
+                System.out.printf("%d ",i);
+            }
         }
     }
 }
