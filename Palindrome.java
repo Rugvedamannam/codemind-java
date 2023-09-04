d1c6ff0c182c51612cc0f38396c64f1671f2fff3
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2023-09-04 12:03:56 +0530

    import java.util.Scanner; public class sample{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int temp=n;         int r,s=0;         while(n!=0)         {             r=n%10;             s=s*10+r;             n=n/10;         }         if(temp==s)         {             System.out.print("True");         }         else         {             System.out.print("False");         }     } }

--- Palindrome.java
+++ Palindrome.java
@@ -3,22 +3,22 @@ public class sample{
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int n,q,r,s=0;
-        n=sc.nextInt();
-        q=n;
+        int n=sc.nextInt();
+        int temp=n;
+        int r,s=0;
         while(n!=0)
         {
             r=n%10;
             s=s*10+r;
             n=n/10;
         }
-       if(s==q)
-       {
-           System.out.print("Palindrome");
-       }
-       else
-       {
-           System.out.print("Not Palindrome");
-       }
+        if(temp==s)
+        {
+            System.out.print("True");
+        }
+        else
+        {
+            System.out.print("False");
+        }
     }
 }
