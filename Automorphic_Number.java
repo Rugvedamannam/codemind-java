6e381ef1618782012ada3990dffe8c5bd2c0ddce
Rugveda Mannam <120186790+Rugvedamannam@users.noreply.github.com>
2023-09-07 10:57:26 +0530

    import java.util.*; public class sample{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int s=0;         s=n*n;         String s1=Integer.toString(n);         String s2=Integer.toString(s);         if(s2.endsWith(s1))         {             System.out.println("Automorphic Number");         }         else         {             System.out.println("Not an Automorphic Number");         }     } }

--- Automorphic_Number.java
+++ Automorphic_Number.java
@@ -1,25 +1,20 @@
-import java.util.Scanner;  
-public class AutomorphicNumberExample2  
-{  
-public static void main(String args[])  
-{  
-Scanner in = new Scanner(System.in);  
-int num = in.nextInt();  
-int count=0;  
-int square = num*num;  
-int temp = num;    
-while(temp>0)  
-{  
-count++;  
-//removes last digit of the variable num  
-temp=temp/10;  
-}   
-//determines the last digit of the variable square  
-int lastDigit = (int) (square%(Math.pow(10, count)));   
-//compare num with last digit of the variable square  
-if(num == lastDigit)  
-System.out.println("Automorphic Number");  
-else  
-System.out.println("Not an Automorphic Number");  
-}  
+import java.util.*;
+public class sample{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int s=0;
+        s=n*n;
+        String s1=Integer.toString(n);
+        String s2=Integer.toString(s);
+        if(s2.endsWith(s1))
+        {
+            System.out.println("Automorphic Number");
+        }
+        else
+        {
+            System.out.println("Not an Automorphic Number");
+        }
+    }
 }
